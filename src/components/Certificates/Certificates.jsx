197b5a653a33e8e45672d0bcc7fd5486f3276067
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-08-16 16:22:20 +0200

    commit

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -1,3 +1,20 @@
+import React, { useState } from 'react';
+import dbCertificate from '../../db/certificates.json';
+import { useTranslation } from 'react-i18next';
+
 export default function Certificates() {
-    
+  const { t, i18n } = useTranslation();
+  const currentLanguage = i18n.language.toUpperCase();
+
+  const data = dbCertificate.map(item => {
+    const languageSpecificData = item.certificate[currentLanguage];
+    return {
+      ...languageSpecificData,
+      id: item.certificate.id,
+      thumbnail: item.certificate.thumbnail,
+      linkCertificate: item.certificate.linkCertificate,
+      startDate: item.certificate.startDate,
+      finishtDate: item.certificate.finishtDate,
+    };
+  });
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,6 +2,7 @@ import PageTitle from 'components/Utility/PageTitle';
 import SubTitle from 'components/Utility/SubTitle';
 import { useTranslation } from 'react-i18next';
 import { BsPersonVcard } from 'react-icons/bs';
+import Certificates from 'components/Certificates/Certificates';
 
 export default function ResumePage() {
   const { t } = useTranslation();
@@ -10,6 +11,7 @@ export default function ResumePage() {
     <>
       <PageTitle icon={<BsPersonVcard />} title={t('resume-page.title')} />
       <SubTitle subTitle={t('resume-page.sub-title')} />
+      <Certificates />
     </>
   );
 }
