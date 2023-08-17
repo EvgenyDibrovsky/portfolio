a8cac2a8e1a8b8667074946bb0e34fe4c6b4a581
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-08-17 13:12:49 +0200

    commit

--- src/components/Certificates/CertificatesListItem.jsx
+++ src/components/Certificates/CertificatesListItem.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
+import ModalImage from 'react-modal-image';
 
 export default function CertificatesListItem({ item }) {
   return (
     <li>
-      <a href={item.linkCertificate} target="_blank" rel="noopener noreferrer">
-        <img
-          src={process.env.PUBLIC_URL + item.thumbnail}
-          alt={item['name-certificate']}
-          className="mx-auto w-full"
-          loading="lazy"
-        />
-        <h3>{item['name-certificate']}</h3>
-        <p>{item.description}</p>
-        <p>{item['name-school']}</p>
-        <p>
-          {item.startDate} - {item.finishtDate}
-        </p>
-      </a>
+      <ModalImage
+        small={process.env.PUBLIC_URL + item.thumbnail}
+        large={process.env.PUBLIC_URL + item.fullOmage}
+        alt="Описание изображения"
+      />
+      <h3>{item['name-certificate']}</h3>
+      <p>{item.description}</p>
+      <p>{item['name-school']}</p>
+      <p>
+        {item.startDate} - {item.finishtDate}
+      </p>
     </li>
   );
 }
