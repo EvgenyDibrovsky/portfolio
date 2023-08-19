deafc55365144da29d6463a9ea438696d73ad618
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-08-19 20:07:32 +0200

    commit

--- src/components/LangSkillsProgressBar/LangSkillsProgressBar.jsx
+++ src/components/LangSkillsProgressBar/LangSkillsProgressBar.jsx
@@ -1,59 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import dbLangSkills from '../../db/langskills.json';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useInView } from 'react-intersection-observer';
+import '/node_modules/flag-icons/css/flag-icons.min.css';
 
 export default function ProgressLangSkills() {
-  const { i18n } = useTranslation();
-  const currentLanguage = i18n.language.toUpperCase();
+  const { t } = useTranslation();
+
+  const skills = useMemo(
+    () => [
+      { value: 30, languageKey: 'english' },
+      { value: 70, languageKey: 'polish' },
+      { value: 100, languageKey: 'ukrainian' },
+      { value: 100, languageKey: 'russian' },
+    ],
+    []
+  );
 
   // Крючок для определения, когда элемент становится видимым
   const [ref, inView] = useInView({
-    triggerOnce: true, // Активировать только один раз
-    threshold: 0.1, // Активировать, когда 10% элемента видны
+    triggerOnce: true,
+    threshold: 0.1,
   });
 
-  const data = dbLangSkills.map(item => {
-    const languageSpecificData = item.languageskills[currentLanguage];
-    return {
-      ...languageSpecificData,
-      id: item.languageskills.id,
-      maxValue: item.languageskills.value,
-    };
-  });
-
-  const [currentValues, setCurrentValues] = useState(data.map(d => 0));
+  const [currentValues, setCurrentValues] = useState(skills.map(d => 0));
 
   useEffect(() => {
-    // Если элемент виден, начать анимацию
     if (inView) {
-      const maxProgressValue = Math.max(...data.map(item => item.maxValue));
+      const maxProgressValue = Math.max(...skills.map(skill => skill.value));
       if (Math.max(...currentValues) < maxProgressValue) {
         const timeout = setTimeout(() => {
           setCurrentValues(prevValues =>
-            prevValues.map((v, index) => Math.min(v + 1, data[index].maxValue))
+            prevValues.map((v, index) => Math.min(v + 1, skills[index].value))
           );
         }, 20);
         return () => clearTimeout(timeout);
       }
     }
-  }, [currentValues, data, inView]);
+  }, [currentValues, skills, inView]);
 
   return (
     <ul className="grid grid-cols lg:grid-cols-2 gap-4" ref={ref}>
-      {data.map((lang, index) => (
-        <li key={lang.id} className="relative h-8 bg-gray-200 w-full rounded-lg px-5">
-          <div
-            className="absolute left-0 top-0 h-8 bg-green-500 rounded-lg"
-            style={{ width: `${currentValues[index]}%` }}
-          ></div>
-
-          <div className="absolute top-0 bottom-0 flex items-center gap-4">
-            <div className="w-full">{lang.language}</div>
-            <div className="w-full">{currentValues[index]}%</div>
+      <li className="relative h-8 bg-gray-200 w-full rounded-lg px-5">
+        <div
+          className="fi fi-us bg-left bg-contain absolute left-0 top-0 h-8 bg-green-500 rounded-lg"
+          style={{ width: `${currentValues[0]}%` }}
+        >
+          <div className="absolute w-full pl-16  top-0 bottom-0 flex justify-between items-center gap-4 px-5 text-black font-medium">
+            <div>{t('languageskills.english')}</div>
+            <div>{currentValues[0]}%</div>
           </div>
-        </li>
-      ))}
+        </div>
+      </li>
+      <li className="relative h-8 bg-gray-200 w-full rounded-lg px-5">
+        <div
+          className="fi fi-pl bg-left bg-contain absolute left-0 top-0 h-8 bg-green-500 rounded-lg"
+          style={{ width: `${currentValues[1]}%` }}
+        >
+          <div className="absolute w-full pl-16 top-0 bottom-0 flex justify-between items-center gap-4 px-5 text-black font-medium">
+            <div>{t('languageskills.polish')}</div>
+            <div>{currentValues[1]}%</div>
+          </div>
+        </div>
+      </li>
+      <li className="relative h-8 bg-gray-200 w-full rounded-lg px-5">
+        <div
+          className="fi fi-ua bg-left bg-contain absolute left-0 top-0 h-8 bg-green-500 rounded-lg"
+          style={{ width: `${currentValues[2]}%` }}
+        >
+          <div className="absolute w-full pl-16 top-0 bottom-0 flex justify-between items-center gap-4 px-5 text-black font-medium">
+            <div>{t('languageskills.ukrainian')}</div>
+            <div>{currentValues[2]}%</div>
+          </div>
+        </div>
+      </li>
+      <li className="relative h-8 bg-gray-200 w-full rounded-lg px-5">
+        <div
+          className="fi fi-ru bg-left bg-contain absolute left-0 top-0 h-8 bg-green-500 rounded-lg"
+          style={{ width: `${currentValues[3]}%` }}
+        >
+          <div className="absolute w-full pl-16  top-0 bottom-0 flex justify-between items-center gap-4 px-5 text-black font-medium">
+            <div>{t('languageskills.russian')}</div>
+            <div>{currentValues[3]}%</div>
+          </div>
+        </div>
+      </li>
     </ul>
   );
 }
