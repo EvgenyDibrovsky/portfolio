b75cbb2609ed706597ece76539931565a4c1c8ba
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-08-28 13:48:18 +0200

    commit

--- src/components/Utility/SubTitle.jsx
+++ src/components/Utility/SubTitle.jsx
@@ -1,3 +1,3 @@
 export default function SubTitle({ subTitle }) {
-  return <h2 className="text-[1.25rem] text-black dark:text-white">{subTitle}</h2>;
+  return <h2 className="text-[1.25rem] text-black dark:text-white text-center md:text-right">{subTitle}</h2>;
 }
