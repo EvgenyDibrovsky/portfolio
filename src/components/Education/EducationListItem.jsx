7fae6f75904a288f6652cdfba7654e6bccee2dfc
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-08-31 09:36:53 +0200

    commit

--- src/components/Education/EducationListItem.jsx
+++ src/components/Education/EducationListItem.jsx
@@ -8,7 +8,7 @@ export default function EducationListItem({ id, bgClass }) {
     <AnimateElements>
       <li className="w-full flex justify-between h-full border border-colorBorder dark:border-colorBorderDark rounded-md opacity-0 init-animate-1">
         <div className="z-10 w-full h-full flex flex-col gap-2 justify-between p-5">
-          {['title', 'faculty', 'speciality', 'form', 'county', 'period'].map(field => (
+          {['title', 'faculty', 'speciality', 'form', 'country', 'period'].map(field => (
             <div key={field}>
               <p className="text-textColor dark:text-white text-[1rem] font-semibold">{t(`education.${field}`)}</p>
               <p className="text-textColor dark:text-white text-[1rem]">{t(`education.${id}.${field}-description`)}</p>
--- src/components/Experience/ExperienceListItem.jsx
+++ src/components/Experience/ExperienceListItem.jsx
@@ -1,18 +1,14 @@
-import { useTranslation } from 'react-i18next';
-
 export default function ExperienceListItem({ data }) {
-  const { t } = useTranslation();
-
   return (
     <li className="relative w-full flex justify-between h-full p-5  border border-colorBorder dark:border-colorBorderDark rounded-md">
       <div>
-        <img src={`${process.env.PUBLIC_URL}${data.logo}`} alt={t(`experience.${data.id}.title-description`)} className="absolute top-5 right-5 w-auto h-6 object-cover rounded" />
+        <img src={`${process.env.PUBLIC_URL}${data.logo}`} alt={data['title-description']} className="absolute top-5 right-5 w-auto h-6 object-cover rounded" />
       </div>
       <div className="w-full flex flex-col gap-2">
-        {['title', 'position', 'responsibilities', 'county', 'period'].map(field => (
+        {['title', 'position', 'responsibilities', 'country', 'period'].map(field => (
           <div key={field}>
-            <p className="text-textColor dark:text-white text-[1rem] font-semibold">{t(`experience.${field}`)}</p>
-            <p className="text-textColor dark:text-white text-[1rem]">{t(`experience.${data.id}.${field}-description`)}</p>
+            <p className="text-textColor dark:text-white text-[1rem] font-semibold">{data[field]}</p>
+            <p className="text-textColor dark:text-white text-[1rem]">{data[`${field}-description`]}</p>
           </div>
         ))}
       </div>
