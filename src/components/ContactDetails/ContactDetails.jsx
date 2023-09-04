2bfe30fd959597bfe143e5904e02bf8c460be50b
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-04 10:46:53 +0200

    commit

--- src/components/ContactDetails/ContactDetails.jsx
+++ src/components/ContactDetails/ContactDetails.jsx
@@ -7,19 +7,19 @@ export default function ContactDetails() {
   return (
     <AnimateElements>
       <ul className="grid grid-cols-1 lg:grid-cols-3 gap-6 justify-center">
-        <li className="flex justify-center items-center gap-4 lg:flex-col xl:flex-row border border-colorBorder dark:border-colorBorderDark py-5 lg:py-10 opacity-0 init-animate-1">
+        <li className="flex justify-center items-center gap-4 lg:flex-col xl:flex-row border border-colorBorder dark:border-colorBorderDark h-20 lg:h-40 opacity-0 init-animate-1">
           <BsPhone className="w-8 h-8 text-orange-400" />
           <a className="text-[1rem] lg:text-[1.25rem] font-semibold text-textColor dark:text-white transition-all duration-200 hover:underline" href={`tel:${t('contact-dedails.phone-number')}`}>
             {t('contact-dedails.phone-number')}
           </a>
         </li>
-        <li className="flex justify-center items-center gap-4 lg:flex-col xl:flex-row border border-colorBorder dark:border-colorBorderDark py-5 lg:py-10 opacity-0 init-animate-1">
+        <li className="flex justify-center items-center gap-4 lg:flex-col xl:flex-row border border-colorBorder dark:border-colorBorderDark h-20 lg:h-40 opacity-0 init-animate-1">
           <BsEnvelope className="w-8 h-8 text-orange-400" />
           <a className="text-[1rem] lg:text-[1.25rem] font-semibold text-textColor dark:text-white duration-200 hover:underline" href={`mailto:${t('contact-dedails.email-address')}`}>
             {t('contact-dedails.email-address')}
           </a>
         </li>
-        <li className="flex justify-center items-center gap-4 lg:flex-col xl:flex-row  border border-colorBorder dark:border-colorBorderDark py-5 lg:py-10 opacity-0 init-animate-1">
+        <li className="flex justify-center items-center gap-4 lg:flex-col xl:flex-row  border border-colorBorder dark:border-colorBorderDark h-20 lg:h-40 opacity-0 init-animate-1">
           <BsPinMap className="w-8 h-8 text-orange-400" />
           <p className="text-[1rem] lg:text-[1.25rem] font-semibold text-textColor dark:text-white">{t('contact-dedails.localization')}</p>
         </li>
