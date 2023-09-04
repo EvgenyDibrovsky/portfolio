127a8fbba811fe3abc2b5370da3f52d076906824
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-04 10:40:20 +0200

    commit

--- src/components/WorkingСonditions/WorkingСonditions.jsx
+++ src/components/WorkingСonditions/WorkingСonditions.jsx
@@ -7,15 +7,15 @@ export default function WorkingСonditions() {
   return (
     <AnimateElements>
       <ul className="grid grid-cols-1 lg:grid-cols-3 gap-6 justify-center">
-        <li className="flex justify-center items-center gap-4 border border-colorBorder dark:border-colorBorderDark py-10 opacity-0 init-animate-1">
+        <li className="flex justify-center items-center gap-4 lg:flex-col xl:flex-row  border border-colorBorder dark:border-colorBorderDark py-5 lg:py-10 opacity-0 init-animate-1">
           <BsGlobe className="w-8 h-8 text-orange-400" />
           <p className="text-[1rem] lg:text-[1.25rem] font-semibold text-textColor dark:text-white mb-2">{t('working-conditions.freelance')}</p>
         </li>
-        <li className="flex justify-center items-center gap-4 border border-colorBorder dark:border-colorBorderDark py-10 opacity-0 init-animate-1">
+        <li className="flex justify-center items-center gap-4 lg:flex-col xl:flex-row border border-colorBorder dark:border-colorBorderDark py-5 lg:py-10 opacity-0 init-animate-1">
           <BsClock className="w-8 h-8 text-orange-400" />
           <p className="text-[1rem] lg:text-[1.25rem] font-semibold text-textColor dark:text-white mb-2">{t('working-conditions.full-employment')}</p>
         </li>
-        <li className="flex justify-center items-center gap-4 border border-colorBorder dark:border-colorBorderDark py-10 opacity-0 init-animate-1">
+        <li className="flex justify-center items-center gap-4 lg:flex-col xl:flex-row border border-colorBorder dark:border-colorBorderDark py-5 lg:py-10 opacity-0 init-animate-1">
           <BsBriefcase className="w-8 h-8 text-orange-400" />
           <p className="text-[1rem] lg:text-[1.25rem] font-semibold text-textColor dark:text-white mb-2">{t('working-conditions.b2b')}</p>
         </li>
