94b9e04506f11d38ac7689b65b737aa442d60d23
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-04 21:00:25 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import Cookies from 'components/Cookies/Cookies';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './index.css';
 import './i18n';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Cookies>
@@ -12,3 +14,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Cookies>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
