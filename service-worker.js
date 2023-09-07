2378459a8ca9baecc6023db02f5ec87e7bf2245f
EvgenyDibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-07 11:05:32 +0000

    Deploying to gh-pages from @ EvgenyDibrovsky/portfolio@6df3cce747513c5f03642b17490b431e1160a113 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-edweb.site-v2';
+const CACHE_NAME = 'cache-edweb.site-v3';
 const urlsToCache = [
   '/',
   '/index.html', // добавьте другие ресурсы, которые вы хотите кешировать
