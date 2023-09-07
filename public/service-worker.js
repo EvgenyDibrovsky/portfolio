13c7c72130fc4327e3153a69faca067344786292
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2023-09-07 19:34:06 +0200

    commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-edweb.site-v6';
+const CACHE_NAME = 'cache-edweb.site-v7';
 const urlsToCache = [
   '/',
   '/index.html', // добавьте другие ресурсы, которые вы хотите кешировать
