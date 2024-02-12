316abd0eab78d0b8387148d1b095ef4d39ce428b
EvgenyDibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-02-12 15:34:44 +0000

    Deploying to gh-pages from @ EvgenyDibrovsky/portfolio@d8a8d97e8b6e6b9d3e84f3abac640d52d65ea8bf 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "cache-edweb.site-v24";
+const CACHE_NAME = 'cache-edweb.site-v25';
 const urlsToCache = [
-  "/",
-  "/index.html", // добавьте другие ресурсы, которые вы хотите кешировать
+  '/',
+  '/index.html', // добавьте другие ресурсы, которые вы хотите кешировать
 ];
 
 // Установка Service Worker и кеширование ресурсов
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
     })
   );
@@ -15,24 +15,24 @@ self.addEventListener("install", (event) => {
 });
 
 // Отлов сетевых запросов
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   // Игнорируем запросы с схемой 'chrome-extension'
-  if (event.request.url.startsWith("chrome-extension://")) {
+  if (event.request.url.startsWith('chrome-extension://')) {
     return;
   }
 
   // Игнорируем POST-запросы
-  if (event.request.method === "POST") {
+  if (event.request.method === 'POST') {
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
+    caches.match(event.request).then(cachedResponse => {
       if (cachedResponse) {
         return cachedResponse;
       }
-      return fetch(event.request).then((response) => {
-        return caches.open(CACHE_NAME).then((cache) => {
+      return fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, response.clone());
           return response;
         });
@@ -42,12 +42,12 @@ self.addEventListener("fetch", (event) => {
 });
 
 // Обновление Service Worker и очистка старых кешей
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
+        cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
