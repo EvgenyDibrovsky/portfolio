8160c6b383f6aedaa7b8ebc708ce67a31dc2ac96
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-02-13 00:15:50 +0100

    commit

--- src/components/Header/NavHeaderItem.jsx
+++ src/components/Header/NavHeaderItem.jsx
@@ -10,7 +10,7 @@ export default function NavMenuItem({ to, labelKey, closeHeader }) {
       <NavLink
         to={to}
         onClick={closeHeader}
-        className="w-full block font-semibold text-black py-4 dark:text-white border-b border-b-colorBorderNavMenu last:border-b-0 text-center group dark:border-b-colorBorderNavMenuDark  duration-200 hover:bg-gradient-to-r hover:from-orange-400 hover:to-orange-300 hover:text-white dark:hover:text-black [&.active]:text-white dark:[&.active]:text-black [&.active]:bg-gradient-to-r [&.active]:from-orange-400 [&.active]:to-orange-300"
+        className="w-full block font-semibold text-black py-4 dark:text-white border-b border-b-colorBorderNavMenu last:border-b-0 text-center group dark:border-b-colorBorderNavMenuDark  duration-200 hover:bg-gradient-to-r hover:bg-orange-400 hover:text-white dark:hover:text-black [&.active]:text-white dark:[&.active]:text-black [&.active]:bg-gradient-to-r [&.active]:bg-orange-400"
       >
         {t(labelKey)}
       </NavLink>
--- src/components/Portfolio/PortfolioFilter.jsx
+++ src/components/Portfolio/PortfolioFilter.jsx
@@ -13,22 +13,13 @@ export default function PortfolioFilter({ onFilterChange }) {
   return (
     <AnimateElements>
       <ul className="flex flex-col lg:flex-row items-center justify-end gap-4 mb-4 w-full xl:w-8/12 2xl:w-6/12 xl:ml-auto opacity-0 init-animate-2" data-time="1.2s" data-delay="0.5s">
-        <li
-          onClick={() => handleFilterChange('all')}
-          className={`btn-filter ${activeFilter === 'all' ? 'bg-gradient-to-r from-orange-300 to-orange-400 text-white dark:text-black' : 'bg-transparent'}`}
-        >
+        <li onClick={() => handleFilterChange('all')} className={`btn-filter ${activeFilter === 'all' ? 'bg-orange-400 text-white dark:text-black' : 'bg-transparent'}`}>
           {t('portfolio.portfolio-filter.all')}
         </li>
-        <li
-          onClick={() => handleFilterChange('educational')}
-          className={`btn-filter ${activeFilter === 'educational' ? 'bg-gradient-to-r from-orange-300 to-orange-400 text-white dark:text-black' : 'bg-transparent'}`}
-        >
+        <li onClick={() => handleFilterChange('educational')} className={`btn-filter ${activeFilter === 'educational' ? 'bg-orange-400 text-white dark:text-black' : 'bg-transparent'}`}>
           {t('portfolio.portfolio-filter.educational')}
         </li>
-        <li
-          onClick={() => handleFilterChange('commercial')}
-          className={`btn-filter  ${activeFilter === 'commercial' ? 'bg-gradient-to-r from-orange-300 to-orange-400 text-white dark:text-black' : 'bg-transparent'}`}
-        >
+        <li onClick={() => handleFilterChange('commercial')} className={`btn-filter  ${activeFilter === 'commercial' ? 'bg-orange-400 text-white dark:text-black' : 'bg-transparent'}`}>
           {t('portfolio.portfolio-filter.commercial')}
         </li>
       </ul>
