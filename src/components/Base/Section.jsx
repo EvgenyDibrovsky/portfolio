30e0d76b04e3a96066a4b4fa24c81e43d7ede806
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-02-13 15:46:46 +0100

    commit

--- src/components/Base/Section.jsx
+++ src/components/Base/Section.jsx
@@ -1,5 +1,5 @@
 export default function Section({ children, customPadding }) {
-  const defaultPadding = 'md:pl-[16.5rem] lg:pl-[12.5rem] pt-24 pb-10 md:py-6';
+  const defaultPadding = 'md:pl-[12.5rem] lg:pl-[12.5rem] pt-24 pb-10 md:py-6';
   const paddingClass = customPadding || defaultPadding;
 
   return <section className={`h-full  ${paddingClass}`}>{children}</section>;
